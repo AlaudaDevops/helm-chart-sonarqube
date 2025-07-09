d037e701b5675dcd4c82f336d97cfdbc5d72c52e
nanjingfm <mingfu@alauda.io>
2025-07-09 18:06:54 +0800

    fix: bump sonarqube scanner version used in the integration test (#41)

--- testing/scripts/scan.sh
+++ testing/scripts/scan.sh
@@ -11,20 +11,17 @@ TEMP_DIR=$(mktemp -d)
 echo "Using temp dir: $TEMP_DIR"
 cp -R "$REPO_PATH" "$TEMP_DIR/repo"
 
-# Check if -Dsonar.login or -Dsonar.token is present in the array
-SONAR_LOGIN_PRESENT=false
+# Check if -Dsonar.token is present in the array
 SONAR_TOKEN_PRESENT=false
 
 for arg in "$@"; do
-    if [[ "$arg" == -Dsonar.login* ]]; then
-        SONAR_LOGIN_PRESENT=true
-    elif [[ "$arg" == -Dsonar.token* ]]; then
+    if [[ "$arg" == -Dsonar.token* ]]; then
         SONAR_TOKEN_PRESENT=true
     fi
 done
 
 # If neither is present, generate a token
-if ! $SONAR_LOGIN_PRESENT && ! $SONAR_TOKEN_PRESENT; then
+if ! $SONAR_TOKEN_PRESENT; then
     echo "Generating token using get_token script"
     url="${SONAR_HOST}/api/user_tokens/generate?name=my-token-$(date +%s)"
     TOKEN=$(curl -s -k -X POST -u "${SONAR_USER}:${SONAR_PWD}" "$url" | jq -r '.token')
@@ -32,7 +29,7 @@ if ! $SONAR_LOGIN_PRESENT && ! $SONAR_TOKEN_PRESENT; then
         echo "Failed to generate token"
         exit 1
     fi
-    set -- "$@" "-Dsonar.login=$TOKEN"
+    set -- "$@" "-Dsonar.token=$TOKEN"
 fi
 
 cd "$TEMP_DIR/repo"
